72b3e304eb49da60f7d793e68d92dd8c178e51f5
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-16 23:34:45 -0500

    generando un id para un token unico

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js'
+import generateId from '../helpers/generateId.js';
 
 const register = async (req, res) => {
     // Evitar registros duplicados 
@@ -12,6 +13,7 @@ const register = async (req, res) => {
 
     try {
         const user = new User(req.body)
+        user.token = generateId();
         const storedUser = await user.save()
         res.json(storedUser);
     } catch (error) {
