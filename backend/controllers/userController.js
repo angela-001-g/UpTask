70ee3e2a3a83ff5d85e83faa64359ef2dd51ae76
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-16 23:04:59 -0500

    previniendo usuarios ducplicados

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,14 @@
 import User from '../models/User.js'
 
 const register = async (req, res) => {
+    // Evitar registros duplicados 
+    const { email } = req.body;
+    const userExists = await User.findOne({ email })
+
+    if(userExists){
+        const error = new Error('Alredy registred user')
+        return res.status(400).json({msg: error.message})
+    }
 
     try {
         const user = new User(req.body)
