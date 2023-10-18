5916481682a52ee7a37e715694d5c2ffa55ba4c7
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-17 23:21:44 -0500

    almacenar el nuevo password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, authenticate, confirm, forgotPassword, checkToken } from '../controllers/userController.js';
+import { register, authenticate, confirm, forgotPassword, checkToken, newPassword } from '../controllers/userController.js';
 
 const router = express.Router(); 
 
@@ -9,7 +9,7 @@ router.post("/", register)  // Crea un nuevo usuario
 router.post("/login", authenticate)
 router.get("/confirm/:token", confirm)
 router.post("/forgot-password", forgotPassword)
-router.get("/forgot-password/:token", checkToken)
+router.route("/forgot-password/:token").get(checkToken).post(newPassword)
 
 
 export default router  
