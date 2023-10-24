68bd211f55e6fbac01c88586dd96b4d977a33703
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-23 21:50:21 -0500

    actualizar una tarea

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -45,7 +45,31 @@ const getTask = async(req, res) => {
 } 
 
 const updateTask = async(req, res) => {
-    
+    const { id } = req.params;
+
+    const task = await Task.findById(id).populate("project")
+
+    if(!task){
+        const error = new Error('Task not found')
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error('Action not allowed')
+        return res.status(403).json({msg: error.message})
+    }
+
+    task.name = req.body.name || task.name;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority; 
+    task.deadline = req.body.deadline || task.deadline;
+
+    try {
+        const storageTask = await task.save()
+        res.json(storageTask)
+    } catch (error) {
+        console.log(error)
+    }
 } 
 
 const deleteTask = async(req, res) => {
