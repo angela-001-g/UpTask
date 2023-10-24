9731eabdfe22e8df6251c03818489fb910a5bf54
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-23 21:26:39 -0500

    agregando tareas a un proyecto existente

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,5 +1,28 @@
+import Project from "../models/Project.js"
+import Task from "../models/Task.js"
 
 const addTask = async(req, res) => {
+    
+    const { project } = req.body;
+
+    const projectExist = await Project.findById(project)
+    
+    if(!projectExist){
+        const error = new Error('Project does not exist')
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(projectExist.creator.toString() !== req.user._id.toString()){
+        const error = new Error('You dont have permissions to add tasks')
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        const storageTask = await Task.create(req.body)
+        res.json(storageTask)
+    } catch (error) {
+        console.log(error)
+    }
 
 } 
 
