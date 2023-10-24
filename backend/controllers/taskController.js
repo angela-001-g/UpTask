4f74d43cbad06917186f17b959adff914f02e790
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-23 21:41:08 -0500

    obtener una tarea y validación

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -14,7 +14,7 @@ const addTask = async(req, res) => {
 
     if(projectExist.creator.toString() !== req.user._id.toString()){
         const error = new Error('You dont have permissions to add tasks')
-        return res.status(404).json({msg: error.message})
+        return res.status(403).json({msg: error.message})
     }
 
     try {
@@ -27,7 +27,21 @@ const addTask = async(req, res) => {
 } 
 
 const getTask = async(req, res) => {
-    
+    const { id } = req.params;
+
+    const task = await Task.findById(id).populate("project")
+
+    if(!task){
+        const error = new Error('Task not found')
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(task.project.creator.toString() !== req.user._id.toString()){
+        const error = new Error('Action not allowed')
+        return res.status(403).json({msg: error.message})
+    }
+
+    res.json(task)
 } 
 
 const updateTask = async(req, res) => {
