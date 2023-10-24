4282e5fb388444fee6071fe777aeabbaa9c26e28
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-24 17:04:20 -0500

    creando la pagina de registrar

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,81 @@
-
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <h1 className="text-violet-600 font-black text-6xl capitalize">Create your account and manage your {''}
+          <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+
+        <div className="my-5">
+          <label 
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="name"
+          >Name</label>
+          <input
+            id="name"
+            type="name"
+            placeholder="Your name"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <div className="my-5">
+          <label 
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Registration email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label 
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >Password</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Registration password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <div className="my-5">
+          <label 
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password2"
+          >Repeat password</label>
+          <input
+            id="password2"
+            type="password"
+            placeholder="Repeat your password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input 
+          type="submit"
+          value="Create Account"
+          className="bg-violet-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-violet-800 transition-colors" 
+        />
+
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >Log In</Link>
+
+      </nav>
+    </>
   )
 }
 
