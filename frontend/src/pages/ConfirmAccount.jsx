078b52ce0c7055e19d77ed2c274d78a3c075578e
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-24 20:18:02 -0500

    Creando la pagina de new password y confirm account

--- frontend/src/pages/ConfirmAccount.jsx
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -2,9 +2,11 @@
 
 const ConfirmAccount = () => {
   return (
-    <div>
-      Confirm Account
-    </div>
+    <>
+      <h1 className="text-violet-600 font-black text-6xl capitalize">Confirm your account and start creating your {''}
+        <span className="text-slate-700">projects</span>
+      </h1>
+    </>
   )
 }
 
--- frontend/src/pages/NewPassword.jsx
+++ frontend/src/pages/NewPassword.jsx
@@ -2,9 +2,33 @@
 
 const NewPassword = () => {
   return (
-    <div>
-      New Password
-    </div>
+    <>
+      <h1 className="text-violet-600 font-black text-6xl capitalize">Recover your password and dont lose acces to your {''}
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+            <label 
+              className="uppercase text-gray-600 block text-xl font-bold"
+              htmlFor="password"
+            >New Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Write your new password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            />
+          </div>
+
+        <input 
+          type="submit"
+          value="Save new password"
+          className="bg-violet-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-violet-800 transition-colors" 
+        />
+
+      </form>
+    </>
   )
 }
 
