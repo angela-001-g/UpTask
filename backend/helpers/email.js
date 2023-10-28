e7373b4b9d7fe8a56e30b02442c8c4a6c27b8934
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-10-27 23:50:38 -0500

    enviando email de bienvenida

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,5 +1,31 @@
 import nodemailer from 'nodemailer'
 
-export const registerEmail = (data) => {
-    console.log("Datos", data)
+export const registerEmail = async (data) => {
+    const { email, name, token } = data
+
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "74edb443937ad1",
+          pass: "e07c669fa8ffd5"
+        }
+    });
+
+    // Información del email 
+
+    const info = await transport.sendMail({
+        from: '"UpTask - Project Manager" <cuentas@uptask.com>',
+        to: email,
+        subject: "UpTask - Confirm your account",
+        text: "Confirm your account in UpTask",
+        html: `<p> Hi ${name}, confirm your account in UpTask </p>
+
+        <p> Your account is almost ready, you just have to confirm it in the following link: </p>
+
+        <a href="${process.env.FRONTEND_URL}/confirm/${token}"> Confirm account </a>
+
+        <p> If you did not create this account, you can ignore this message </p>
+        `
+    })
 }
