c13ce9125a77930e968244f83f3a96498ddae51c
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-02 00:19:36 -0500

    autenticanto al usuario

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,6 +20,17 @@ const Login = () => {
       return
     }
 
+    try {
+      const { data } = await clientAxios.post('/users/login', {email, password})
+      setAlert({})
+      localStorage.setItem('token', data.token)
+    } catch (error) {
+      setAlert({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+
   }
 
   const { msg } = alert
