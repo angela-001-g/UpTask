5c6b201f14dd51c60ab72aeaf5e679076489b329
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-02 14:25:49 -0500

    revisando el token para autenticar el usuario

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,11 +1,36 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import clientAxios from "../config/clientAxios";
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
 
     const [auth, setAuth] = useState({})
+
+    useEffect(() => {
+        const authenticateUser = async () => {
+            const token = localStorage.getItem('token')
+            if(!token){
+                return
+            }
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            try {
+                const { data } = await clientAxios('/users/profile', config)
+                setAuth(data)
+            } catch (error) {
+                
+            }
+        }
+        authenticateUser()
+    }, [])
 
     return (
         <AuthContext.Provider
