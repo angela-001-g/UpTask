7d2467bbb504b3cc7ab75ea5cd919d7130e5127a
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-02 19:33:34 -0500

    obteniendo los proyectos del usuario

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -3,13 +3,14 @@ import useProjects from "../hooks/useProjects"
 const Projects = () => {
 
   const { projects } = useProjects()
+  console.log(projects)
 
   return (
     <>
       <h1 className="text-4xl font-black">Projects</h1>
 
-      <div>
-        
+      <div className="bg-white shadow mt-10 rounded-lg">
+        {projects.length ? <p>Si hay projectos</p>: <p className=" text-center text-gray-600 uppercase p-5">No hay proyectos</p>}
       </div>
     </>
   )
