dca13b568c064c4402b6f5f06c897a7b4c9a51ed
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-20 22:35:14 -0500

    Mostrar etiqueta de colaborador si no es proyecto propio

--- frontend/src/components/PreviewProject.jsx
+++ frontend/src/components/PreviewProject.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 const PreviewProject = ({project}) => {
 
-    const {name, _id, customer} = project
+    const { auth } = useAuth()
+
+    const {name, _id, customer, creator } = project
 
   return (
     <>
-        <div className="border-b p-5 flex">
-            <p className="flex-1">{name}
-                <span className="text-sm text-gray-500 uppercase">{' '}{customer}</span>
-            </p>
+        <div className="border-b p-5 flex justify-between">
+            <div className="flex items-center gap-2">
+                <p className="flex-1">{name}
+                    <span className="text-sm text-gray-500 uppercase">{' '}{customer}</span>
+                </p>
+                
+                {auth._id !== creator && (
+                    <p className="p-1 text-xs rounded-lg text-white bg-green-300 font-bold uppercase">Collaborator</p>
+                )}
+            </div>
+
             <Link
                 to={`${_id}`}
                 className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
