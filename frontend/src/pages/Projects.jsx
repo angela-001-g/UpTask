e5945721cf46741d2cf2766dbc3b9b988074a903
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-22 12:35:34 -0500

    conectarse a socket.io

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,10 +1,18 @@
 import useProjects from "../hooks/useProjects"
+import { useEffect } from "react"
 import PreviewProject from "../components/PreviewProject"
 import Alert from "../components/Alert"
+import io from 'socket.io-client'
+
+let socket; 
 
 const Projects = () => {
 
   const { projects, alert } = useProjects()
+
+  useEffect(() => {
+    socket = io(import.meta.env.VITE_BACKEND_URL)
+  }, [])
 
   const { msg } = alert
 
