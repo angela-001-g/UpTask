f18d7f8c1600c5e649f63c19728ba9d808567c2e
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-11-30 19:31:14 -0500

    error socket

--- backend/index.js
+++ backend/index.js
@@ -61,8 +61,6 @@ io.on('connection', (socket) => {
         socket.join(project)
     })
 
-    // Dear Lord When I GTH
-
     socket.on('new task', (task) => {
         console.log('aqui')
         console.log(task)
